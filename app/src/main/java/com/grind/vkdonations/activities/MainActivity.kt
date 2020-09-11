ca28ca50f423d94202310f7d9b6abda5e51e3c5b
grindwork <grindwork@mail.ru>
2020-09-11 23:56:02 +0300

    half layouts

--- app/src/main/java/com/grind/vkdonations/activities/MainActivity.kt
+++ app/src/main/java/com/grind/vkdonations/activities/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.grind.vkdonations.activities
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.HandlerThread
+import android.renderscript.ScriptGroup
 import com.grind.vkdonations.R
+import java.util.concurrent.Callable
+import java.util.concurrent.Future
+import java.util.concurrent.FutureTask
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
