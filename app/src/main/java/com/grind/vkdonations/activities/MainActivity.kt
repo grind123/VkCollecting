d1405a7abe862fca9073413ed0e63f4127d4de7a
grindwork <grindwork@mail.ru>
2020-09-11 15:36:02 +0300

    init commit

---
+++ app/src/main/java/com/grind/vkdonations/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.grind.vkdonations.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grind.vkdonations.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
